85211149c195238c92015e0e13b5a551cfe3d5c6
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2023-02-28 01:17:49 +0100

    Deploying the backend

--- online-shop-backend/index.js
+++ online-shop-backend/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Welcome to our online Shop API...');
+    res.sendStatus(200);
 });
 
 app.get('/products', (req, res) => {
